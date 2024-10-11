aa71dd1bb2a83c0262b25445b876048130306aab
Giffi <miklas.karjalainen@gmail.com>
2024-10-11 11:17:22 +0300

    more semantics

--- src/semantics.c
+++ src/semantics.c
@@ -11,21 +11,43 @@
 #include "compile_error.h"
 
 #define ANALYZER_ERROR(position, ...) do { if (position.filepath ) { PRINT_ERROR_IN_FILE(position, __VA_ARGS__); LONGJUMP(1); } else { PANIC(__VA_ARGS__); } } while(0)
-#define ANALYZE_BODY(body) do {             \
+#define CALL_ON_BODY(fn, body, ...) do {    \
     const size_t Length = arrlenu(body);    \
     for (size_t i = 0; i < Length; i++) {   \
-        _analyze_node(body[i], analyzer);   \
+        fn(body[i], __VA_ARGS__);           \
     }                                       \
 } while(0)
 
 typedef struct semantic_analyzer_t {
-    sym_table_t functions, variables, structs;
+    sym_table_t functions, structs;
 } semantic_analyzer_t;
 
-static void _analyze_node(ast_node_t* node, semantic_analyzer_t* analyzer) {
+static void _analyze_scoped_node(ast_node_t* node, semantic_analyzer_t* analyzer, sym_table_t* variables) {
+    UNUSED(analyzer);
+
+    switch (node->kind) {
+        case AST_VARIABLE_DECLARATION: {
+            {
+                ast_variable_declaration_t* var_decl = sym_table_get(variables, node->data.variable_declaration.name);
+                if (var_decl){
+                    ANALYZER_ERROR(node->position, "Variable called '%s' is already defined!", node->data.variable_declaration.name);
+                }
+            }
+            sym_table_insert(variables, node->data.variable_declaration.name, (void*)&node->data.variable_declaration);
+
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+}
+
+static void _analyze_global_node(ast_node_t* node, semantic_analyzer_t* analyzer) {
     switch (node->kind) {
         case AST_TRANSLATION_UNIT: {
-            ANALYZE_BODY(node->data.translation_unit.body);
+            CALL_ON_BODY(_analyze_global_node, node->data.translation_unit.body, analyzer);
+            break;
         }
 
         case AST_FUNCTION_DECLARATION: { 
@@ -36,6 +58,16 @@ static void _analyze_node(ast_node_t* node, semantic_analyzer_t* analyzer) {
                 }
             }
             sym_table_insert(&analyzer->functions, node->data.function_declaration.name, node);
+
+            sym_table_t variables;
+            sym_table_init(&variables);
+            const size_t Size = arrlenu(node->data.function_declaration.args);
+            for (size_t arg = 0; arg < Size; arg++) {
+                const ast_variable_declaration_t* Argument = &node->data.function_declaration.args[arg]; 
+                sym_table_insert(&variables, Argument->name, (void*)Argument);
+            }
+            CALL_ON_BODY(_analyze_scoped_node, node->data.function_declaration.body, analyzer, &variables);
+            sym_table_cleanup(&variables);
             break;
         }
 
@@ -49,9 +81,11 @@ static void _analyze_node(ast_node_t* node, semantic_analyzer_t* analyzer) {
 void semantic_analysis(ast_node_t* node) {
     semantic_analyzer_t analyzer = { 0 };
     sym_table_init(&analyzer.functions);
-    sym_table_init(&analyzer.variables);
     sym_table_init(&analyzer.structs);
 
-    _analyze_node(node, &analyzer);
+    _analyze_global_node(node, &analyzer);
+
+    sym_table_cleanup(&analyzer.functions);
+    sym_table_cleanup(&analyzer.structs);
 }
 
