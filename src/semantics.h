1ec00c1b8403c5c96c2d64cf625912a750ede026
Giffi <miklas.karjalainen@gmail.com>
2024-10-25 17:54:27 +0300

    Update semantics.h

--- src/semantics.h
+++ src/semantics.h
@@ -4,6 +4,7 @@
 struct ast_node_t;
 struct arena_t;
 
+// ast is not passed as constant because the type information is set for the ast node.
 void semantic_analysis(struct arena_t* arena, struct ast_node_t* ast_root);
 
 #endif
